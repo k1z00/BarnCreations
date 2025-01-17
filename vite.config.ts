9ac60e39941033aefb2358f2469532eb5af41774
Kirill <kvn2001@internet.ru>
2025-01-17 19:38:13 +0300

    deploy4

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
+
 
 export default defineConfig({
+  base:'/BarnCreations/',
   plugins: [react()],
-  base: '/BarnCreations/',
   css: {
     preprocessorOptions: {
       sass: {
         api: 'modern-compiler' 
       }
     }, 
-  },resolve: {
-    alias: {
-      '@assets': path.resolve(__dirname, 'src/assets'),
-    },
   },
 });
